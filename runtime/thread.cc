70137755ca821e39d8a48c08ce2f106a974b68e8
Yi Kong <yikong@google.com>
2021-08-27 17:31:20 +0800

    Suppress three counts of compiler warnings for frame size limit exceeded
    -Wframe-larger-than= was not enforced when LTO is enabled, until the
    upcoming compiler update. Suppress the warnings for now to unblock the
    update process, and then the ART team can reproduce the breakage.
    
    Bug: 197647048
    Test: build
    Change-Id: Ief1d999efaff4e92a1e44b81d56afec87ee43e72

--- runtime/thread.cc
+++ runtime/thread.cc
@@ -4126,6 +4126,10 @@ void Thread::VisitReflectiveTargets(ReflectiveValueVisitor* visitor) {
   }
 }
 
+// FIXME: clang-r433403 reports the below function exceeds frame size limit.
+// http://b/197647048
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wframe-larger-than="
 template <bool kPrecise>
 void Thread::VisitRoots(RootVisitor* visitor) {
   const uint32_t thread_id = GetThreadId();
@@ -4187,6 +4191,7 @@ void Thread::VisitRoots(RootVisitor* visitor) {
     visitor->VisitRootIfNonNull(&entry.second.this_object_, RootInfo(kRootVMInternal, thread_id));
   }
 }
+#pragma GCC diagnostic pop
 
 void Thread::SweepInterpreterCache(IsMarkedVisitor* visitor) {
   for (InterpreterCache::Entry& entry : GetInterpreterCache()->GetArray()) {
@@ -4223,6 +4228,10 @@ void Thread::SweepInterpreterCache(IsMarkedVisitor* visitor) {
   }
 }
 
+// FIXME: clang-r433403 reports the below function exceeds frame size limit.
+// http://b/197647048
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wframe-larger-than="
 void Thread::VisitRoots(RootVisitor* visitor, VisitRootFlags flags) {
   if ((flags & VisitRootFlags::kVisitRootFlagPrecise) != 0) {
     VisitRoots</* kPrecise= */ true>(visitor);
@@ -4230,6 +4239,7 @@ void Thread::VisitRoots(RootVisitor* visitor, VisitRootFlags flags) {
     VisitRoots</* kPrecise= */ false>(visitor);
   }
 }
+#pragma GCC diagnostic pop
 
 class VerifyRootVisitor : public SingleRootVisitor {
  public:
